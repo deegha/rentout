45ee7a552378591b0ee2bdd54259b21a81471a7b
deegha <shuboothi@gmail.com>
2024-03-25 21:20:15 +0530

    Adding allowed origin

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
 		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "https://renout-ui.vercel.app",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
